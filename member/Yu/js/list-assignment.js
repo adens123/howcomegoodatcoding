db516e8caffa2861387704651a3ff90bba03ab53
yuch3n <dustchen1126@gmail.com>
2023-09-21 22:59:13 +0800

    0921頁面上傳

--- member/Yu/js/list-assignment.js
+++ member/Yu/js/list-assignment.js
@@ -17,8 +17,15 @@ const assignments = [
   },
   {
     date: "2023/09/20",
-    chapter: "Boolean, Undefined, Null、logical, comparison operator ",
+    chapter:
+      "Boolean, Undefined, Null、logical, comparison operator, if statement",
     link: "20230920.html",
+  },
+  {
+    date: "2023/09/21",
+    chapter:
+      "Truthy and Falsy Values, Logical Operator與其他資料類型處理, Coding Convention and Restrictions",
+    link: "20230921.html",
   },
 ];
 
@@ -31,7 +38,7 @@ for (let i = 0; i < assignments.length; i++) {
   <th scope="row">${i + 1}</th>
   <td>${assignments[i].date}</td>
   <td>${assignments[i].chapter}</td>
-  <td><a class="btn btn-outline-secondary" href="./2023-full-stack/${
+  <td><a class="btn btn-outline-secondary text-nowrap" href="./2023-full-stack/${
     assignments[i].link
   }">點此前往</a></td>
 </tr>`;
