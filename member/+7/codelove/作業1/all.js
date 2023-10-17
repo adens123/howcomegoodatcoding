6506524c7478e8af35db965c6ecda4aaa8edb791
generaLra <wj4y7fu6@gmail.com>
2023-10-17 23:59:20 +0800

    loveCode練習到第4課

--- member/+7/codelove/作業1/all.js
+++ member/+7/codelove/作業1/all.js
@@ -1,6 +1,15 @@
 function ibuy() {
   let data = document.getElementById("keyword").value;
   document.getElementById("custom-name").textContent = data;
+
+  let classification = document.getElementById("Classification"); // select (先選到整個
+  let data2 = classification.options[classification.selectedIndex].textContent; // option (再索引單個)
+  document.getElementById("Classific").textContent = data2;
+
+  let type = document.getElementById("type");
+  let data3 = type.options[type.selectedIndex].textContent;
+  document.getElementById("type3").textContent = data3; //先選到type3這個欄位，最後讓它列印出data3的"值"(=客人輸入的資料)
+
   alert(
     data + "謝謝您對我們的衣服有興趣！請致電 0987-654-321，會有專人提供您報價！"
   );
