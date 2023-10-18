d00549d4acb3a767372756959e6f055409dd7add
yuch3n <dustchen1126@gmail.com>
2023-10-18 15:13:55 +0800

    codelove.html, project.html 頁面調整

---
+++ member/Yu/js/codelove.js
@@ -0,0 +1,176 @@
+const codelove_01 = [
+  {
+    ch_num: 1,
+    chapter: "JavaScript（一）幫衣服工廠做個好用表單",
+    lesson: [
+      {
+        date: "2023/09/22",
+        content: "基本的函數操作、onclick 事件",
+        link: "https://jsfiddle.net/kulimusoda/mk2ptwc5/26/",
+      },
+      {
+        date: "2023/09/25",
+        content: "基本的變數操作、從 html 取得內容",
+        link: "https://jsfiddle.net/kulimusoda/cuh8wyrL/104/",
+      },
+      {
+        date: "2023/09/27",
+        content: "修改 html 的內容",
+        link: "https://jsfiddle.net/kulimusoda/wuLntymf/23/",
+      },
+      {
+        date: "2023/09/28",
+        content: "基本的陣列操作",
+        link: "https://jsfiddle.net/kulimusoda/gqxut4cr/58/",
+      },
+      {
+        date: "2023/09/29",
+        content: "基本的 if/else 條件流程控制",
+        link: "https://jsfiddle.net/kulimusoda/sv36cquf/8/",
+      },
+      {
+        date: "2023/10/02",
+        content: "認識資料型態與轉換",
+        link: "https://jsfiddle.net/kulimusoda/8men7y34/26/",
+      },
+      {
+        date: "2023/10/04",
+        content: "認識巢狀結構",
+        link: "https://jsfiddle.net/kulimusoda/p2Lv917r/13/",
+      },
+    ],
+  },
+  {
+    ch_num: 2,
+    chapter: "JavaScript（二）親手做個待辦事項小工具",
+    lesson: [
+      {
+        date: "2023/10/09",
+        content: "認識 DOM 樹、新增元素",
+        link: "https://jsfiddle.net/kulimusoda/Ls8qv6zk/122/",
+      },
+      {
+        date: "2023/10/11",
+        content: "從 DOM 樹移除元素、動態加上 onclick 事件",
+        link: "https://jsfiddle.net/kulimusoda/Ls8qv6zk/200/",
+      },
+      {
+        date: "2023/10/12",
+        content: "認識 for 迴圈",
+        link: "https://jsfiddle.net/kulimusoda/Ls8qv6zk/277/",
+      },
+    ],
+  },
+];
+
+// Chapter render
+function createChapter(course) {
+  let el = document.querySelector("#accordionCourse");
+  let result = "";
+
+  for (let [index, value] of course.entries()) {
+    if (index == 0) {
+      result += `<div class="accordion-item">
+  <h3 class="accordion-header">
+    <button
+      class="accordion-button chapter"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#collapseCh${value.ch_num}"
+      aria-expanded="true"
+      aria-controls="collapseCh${value.ch_num}"
+    >${value.chapter}</button>
+  </h3>
+  <div
+    id="collapseCh${value.ch_num}"
+    class="accordion-collapse collapse show"
+    data-bs-parent="#accordionCourse"
+  >
+    <div class="accordion-body">
+    <table
+    class="table table-hover text-center align-middle"
+  >
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">日期</th>
+        <th scope="col">內容</th>
+        <th scope="col">連結</th>
+      </tr>
+    </thead>
+    <tbody class="course_table">
+    </tbody>
+  </table>
+    </div>
+  </div>
+</div>`;
+    } else {
+      result += `<div class="accordion-item">
+  <h3 class="accordion-header">
+    <button
+      class="accordion-button collapsed chapter"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#collapseCh${value.ch_num}"
+      aria-expanded="true"
+      aria-controls="collapseCh${value.ch_num}"
+    >${value.chapter}</button>
+  </h3>
+  <div
+    id="collapseCh${value.ch_num}"
+    class="accordion-collapse collapse"
+    data-bs-parent="#accordionCourse"
+  >
+    <div class="accordion-body"><table
+    class="table table-hover text-center align-middle"
+  >
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">日期</th>
+        <th scope="col">內容</th>
+        <th scope="col">連結</th>
+      </tr>
+    </thead>
+    <tbody class="course_table">
+    </tbody>
+  </table></div>
+  </div>
+</div>`;
+    }
+  }
+  el.innerHTML = result;
+}
+
+// render course
+function createTable(course) {
+  let el = document.querySelectorAll(".course_table");
+  let result = "";
+  // console.log(el);
+  for (let i = 0; i < course.length; i++) {
+    console.log(course[i].lesson.length);
+    for (let j = 0; j < course[i].lesson.length; j++) {
+      result += `<tr>
+      <th scope="row">${j + 1}</th>
+      <td class="text-secondary">${course[i].lesson[j].date}</td>
+      <td>${course[i].lesson[j].content}</td>
+      <td>
+        <a
+          href="${course[i].lesson[j].link}"
+          class="btn btn-outline-secondary text-nowrap" target="_blank"
+          >點此前往</a
+        >
+      </td>
+    </tr>`;
+    }
+    el[i].innerHTML = result;
+    result = "";
+  }
+}
+
+function render() {
+  createChapter(codelove_01);
+  createTable(codelove_01);
+}
+
+render();
