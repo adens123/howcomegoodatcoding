b6ef4d6855121037721411787e11e6846e043b57
adens123 <adens123@gmail.com>
2023-10-20 17:01:43 +0800

    update codelove jsbasic3-2

--- member/Jacky/codelove/jsbasic3/all.js
+++ member/Jacky/codelove/jsbasic3/all.js
@@ -5,3 +5,23 @@ document.querySelectorAll(".btn-close").forEach(btn => {
     e.target.closest(".alert").remove();
   });
 });
+
+function toastSuccess() {
+  const toastSuccess = document.querySelector(".toast-success");
+
+  toastSuccess.style.display = "inline-block";
+
+  setTimeout(function () {
+    toastSuccess.style.display = "none";
+  }, 5000);
+}
+
+function toastError() {
+  const toastError = document.querySelector(".toast-error");
+
+  toastError.style.display = "inline-block";
+
+  setTimeout(function () {
+    toastError.style.display = "none";
+  }, 5000);
+}
