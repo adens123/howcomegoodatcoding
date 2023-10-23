8565ac34a7094e08eebdfb3427028a43dd79a5d8
Jacky <adens123@gmail.com>
2023-10-23 21:43:40 +0800

    update codelove jsbasic3-3

--- member/Jacky/codelove/jsbasic3/all.js
+++ member/Jacky/codelove/jsbasic3/all.js
@@ -2,26 +2,47 @@ document.querySelectorAll(".btn-close").forEach(btn => {
   btn.addEventListener("click", e => {
     e.preventDefault();
     // e.target.closest(".alert").style.display = "none";
-    e.target.closest(".alert").remove();
+    if (e.target.closest(".alert")) {
+      e.target.closest(".alert").remove();
+    }
   });
 });
 
+function getNode(nodeName) {
+  return document.querySelector(nodeName);
+}
+
+// toast
+
+function toast(node, delayTime) {
+  node.style.display = "inline-block";
+  setTimeout(() => (node.style.display = "none"), delayTime);
+}
+
 function toastSuccess() {
-  const toastSuccess = document.querySelector(".toast-success");
-
-  toastSuccess.style.display = "inline-block";
-
-  setTimeout(function () {
-    toastSuccess.style.display = "none";
-  }, 5000);
+  toast(getNode(".toast-success"), 5000);
 }
 
 function toastError() {
-  const toastError = document.querySelector(".toast-error");
-
-  toastError.style.display = "inline-block";
-
-  setTimeout(function () {
-    toastError.style.display = "none";
-  }, 5000);
+  toast(getNode(".toast-error"), 5000);
 }
+
+// modal
+
+function loginModal() {
+  getNode("#login-modal").style.left = "0";
+}
+
+function postModal() {
+  getNode("#post-modal").style.left = "0";
+}
+
+function modalClose(event) {
+  if (event.target.closest(".modal")) {
+    event.target.closest(".modal").style.left = "-100%";
+  }
+}
+
+getNode(".modal .btn-close").onclick = modalClose;
+
+window.onclick = modalClose;
