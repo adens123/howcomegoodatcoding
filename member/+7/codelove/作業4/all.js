565c90bdf104361ae287e6849592d18913c1a394
generaLra <wj4y7fu6@gmail.com>
2023-12-01 21:41:07 +0800

    loveCode 4-6 ok

--- member/+7/codelove/作業4/all.js
+++ member/+7/codelove/作業4/all.js
@@ -4,21 +4,38 @@
 autosize(document.querySelector("textarea"));
 
 // Init
-let ta = document.querySelector("textarea");
+
+let ta = document.querySelector("form");
 ta.style.display = "none";
 autosize(ta);
 
-// Change layout
+// // Change layout
 ta.style.display = "block";
 autosize.update(ta);
 
-// Change value
+// // Change value
 ta.value = "Some new value";
 autosize.update(ta);
 
-/*4-2 img載入*/
-let lazyLoadInstance = new LazyLoad({
-  // Your custom settings go here
-});
+//4-5
 
-/**/
+$(document).ready(function () {
+  $("#date_timepicker_start").datetimepicker({
+    format: "Y/m/d",
+    // onShow: function (ct) {
+    //   this.setOptions({
+    //     maxDate: $("#date_timepicker_end").val() ? jQuery("#date_timepicker_end").val() : false,
+    //   });
+    // },
+    timepicker: false,
+  });
+  $("#date_timepicker_end").datetimepicker({
+    format: "Y/m/d",
+    // onShow: function (ct) {
+    //   this.setOptions({
+    //     minDate: $("#date_timepicker_start").val() ? jQuery("#date_timepicker_start").val() : false,
+    //   });
+    // },
+    timepicker: false,
+  });
+});
