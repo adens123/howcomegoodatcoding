4ae1a0c1cf11ff0c0c49a56a2d2bbe14149ff379
generaLra <wj4y7fu6@gmail.com>
2023-12-09 00:56:16 +0800

    loveCode 5-1 OK

--- member/+7/codelove/作業5/all.js
+++ member/+7/codelove/作業5/all.js
@@ -1,8 +1,21 @@
 //overlay預設隱藏
 //一進網頁，modal會出現
-//no > 轉址到google首頁
 //yes > (.warn視窗)關閉，看到頁面圖片
+//no > 轉址到google首頁
 
-let modal = document.querySelector(".mode");
+function yes() {
+  document.cookie = "Adult=true";
+  let overlay = document.querySelector(".overlay");
+  overlay.style.display = "none";
+}
 
-function no() {}
+function no() {
+  window.location.href = "https://www.google.com/";
+}
+
+if (document.cookie) {
+  //如果有偵測到cookie，就還原到網頁上
+  //只有ture，所以不做if eles驗證
+  let overlay = document.querySelector(".overlay");
+  overlay.style.display = "none";
+}
